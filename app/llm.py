2b8b2c5539394f9704a041b1e0acd0ca6556ccb5
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2026-01-17 18:11:14 -0300

    Refactor API key handling and update ask_gemini function

--- app/llm.py
+++ app/llm.py
@@ -1,28 +1,30 @@
 import os
-import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as genai
 
+
 load_dotenv()
 
-API_KEY = os.getenv("GEMINI_API_KEY") or st.secrets.get("GEMINI_API_KEY")
-
+API_KEY = os.getenv("GEMINI_API_KEY") 
 if not API_KEY:
-    raise RuntimeError("Defina GEMINI_API_KEY no .env ou nos secrets")
+    raise RuntimeError("GEMINI_API_KEY não encontrada no .env ou nos secrets")
+
 
 genai.configure(api_key=API_KEY)
 
-
-
-
-
 def ask_gemini(prompt: str, context: str = "") -> str:
+    full_prompt = f"""
+Você é um assistente inteligente e objetivo.
+
+PROMPT ATUAL:
+{prompt}
+
+CONTEXTO DA VECTOR STORE:
+{context}
+"""
     try:
-        full_prompt = f"{context}\n{prompt}" if context else prompt
-        response = genai.generate_content(
-            model="models/gemini-2.5-flash-preview-09-2025",
-            contents=full_prompt
-        )
+        model = genai.GenerativeModel("models/gemini-2.0-flash")  
+        response = model.generate_content(full_prompt)
         return response.text
     except Exception as e:
         return f"Erro ao consultar Gemini: {e}"
